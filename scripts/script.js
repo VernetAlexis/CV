bae92f2f30f44a328c141929d6c482fbc925ae95
Alexis Vernet <vernet.alexis01@gmail.com>
2022-04-05 05:06:13 +0200

    Fin du menu de navigation

--- scripts/script.js
+++ scripts/script.js
@@ -10,24 +10,32 @@ let navbarState = 'close'
 navbarButton.addEventListener('click', function() {
     if (navbarState === 'close') {
         sideNavbar.style.transform = 'translateX(0px)'
-        buttonBurgerToCross()
-        profilPicture.style.zIndex = 0;
+        hideBurgerButton()
+        setTimeout(buttonBurgerToCross, 1000)
+        setTimeout(revealCrossButton, 1050)
         navbarState = 'open'
     } else if (navbarState === 'open') {
         sideNavbar.style.transform = 'translateX(-350px)'
-        buttonCrossToBurger()
-        setTimeout(upProfilPicture, 1000)
+        hideCrossButton()
+        setTimeout(buttonCrossToBurger, 1000)
+        setTimeout(revealBurgerButton, 1050)
         navbarState = 'close'
     }
 })
 
+function hideBurgerButton() {
+    navbarButton.style.opacity = '0'
+    navbarButton.style.transition = '1000ms'
+}
+
 function buttonBurgerToCross () {
-    navbarButton.style.right = '30px'
-    navbarButton.style.top = '100px'
+    navbarButton.style.left = '266px'
     navbarButton.style.border = 'solid 2px white'
     navbarButton.style.borderRadius = '50%'
     navbarButton.style.width = '50px'
     navbarButton.style.height = '50px'
+    navbarButton.style.transition = '50ms'
+    navbarButton.style.zIndex = '2'
     buttonIconTop.style.transform = 'translateX(-50%) rotate(45deg)'
     buttonIconTop.style.top = '23px'
     buttonIconTop.style.left = '25px'
@@ -37,14 +45,26 @@ function buttonBurgerToCross () {
     buttonIconMiddle.style.visibility = 'hidden'
 }
 
-function buttonCrossToBurger () {
-    navbarButton.style.right = '0px'
+function revealCrossButton() {
+    navbarButton.style.top = '100px'
+    navbarButton.style.opacity = '1'
+    navbarButton.style.transition = '500ms'
+}
+
+function hideCrossButton() {
     navbarButton.style.top = '182px'
+    navbarButton.style.opacity = '0'
+    navbarButton.style.transition = '200ms'
+}
+
+function buttonCrossToBurger () {
+    navbarButton.style.left = '-132px'
     navbarButton.style.borderLeft = 'none'
     navbarButton.style.borderTop = 'none'
     navbarButton.style.borderRadius = '0 0 100% 0'
     navbarButton.style.width = '130px'
     navbarButton.style.height = '130px'
+    navbarButton.style.transition = '50ms'
     buttonIconTop.style.transform = 'translateX(0) rotate(0deg)'
     buttonIconTop.style.top = '50px'
     buttonIconTop.style.left = '30px'
@@ -52,13 +72,11 @@ function buttonCrossToBurger () {
     buttonIconBottom.style.top = '70px'
     buttonIconBottom.style.left = '30px'
     buttonIconMiddle.style.visibility = 'visible'
-    setTimeout(revealNavbarButton, 1000)
 }
 
-function revealNavbarButton () {
-    navbarButton.style.right = '-132px'
-}
-
-function upProfilPicture () {
-    profilPicture.style.zIndex = 1;
+function revealBurgerButton() {
+    navbarButton.style.opacity = '1'
+    navbarButton.style.transition = '500ms'
+    navbarButton.style.zIndex = '0'
+    navbarButton.style.left = '0px'
 }
